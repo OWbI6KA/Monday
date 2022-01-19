ddb8b3d1702643ff06abbb26fc489410a3508bbe
Ivan040602 <idsvininnikov@edu.hse.ru>
2022-01-19 15:05:52 +0300

    Добавление подпроекта base

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import redirect, render
 
-def register (request):
-    context = {}
-    return render(request,'accounts/register.html', context)
 
-def login_view (request):
+def register(request):
     context = {}
-    return render(request,'accounts/login.html', context)
+    return render(request, 'accounts/register.html', context)
 
-def logout_view (request):
+
+def login_view(request):
+    context = {}
+    return render(request, 'accounts/login.html', context)
+
+
+def logout_view(request):
     return redirect('accounts : login')
