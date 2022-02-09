af41b14b8bcd253e033d720e3d817a9d85c44a02
Kirya <samoylenkk@inbox.ru>
2022-02-09 17:45:07 +0300

    База данных

--- cabinet/views.py
+++ cabinet/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import monday_data
 
 
 def general(request):
-    return render(request, 'cabinet/general.html')
+    x1 = monday_data.objects.all()
+    print(x1)
+    return render(request, 'cabinet/general.html', {"tasks": x1})
 
 
 def private(request):
     return render(request, 'cabinet/private.html')
+
+
+class TasksView(ListView):
+    model = monday_data
+    template_name = 'cabinet/general.html'
+    context_object_name = 'work'
--- database.php
+++
@@ -1,5 +0,0 @@
-<?php
-$db_host = 'localhost';
-$db_name = 'db.sqlite3';
-$db_user = 'root';
-
