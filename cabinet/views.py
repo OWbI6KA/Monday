31b1e8ff5cdc033387b8a56def1df1acf2dfb4ec
samoy <samoylenkk@inbox.ru>
2022-02-15 15:48:56 +0300

    ОНО РАБОТАЕТ
    # Conflicts:
    #	cabinet/views.py
    #	db.sqlite3
    #	templates/cabinet/base.html

--- cabinet/views.py
+++ cabinet/views.py
@@ -3,12 +3,13 @@ from django.shortcuts import render, redirect
 from .models import monday_data
 from accounts.views import usersDirectors
 
+_data = monday_data.objects.all()
+
 
 def general(request):
     if request.user.is_authenticated:
         _name = request.user.first_name
-        workTask = monday_data.objects.all()
-        return render(request, 'cabinet/general.html', {'myName': _name, 'workTask': workTask})
+        return render(request, 'cabinet/general.html', {'myName': _name, 'myData': _data})
     else:
         return redirect('home_page:home')
 
@@ -16,9 +17,8 @@ def general(request):
 def private(request):
     if request.user.is_authenticated:
         _name = request.user.first_name
-        workTask = monday_data.objects.all()
         if request.user in usersDirectors:
-            return render(request, 'cabinet/private.html', {'myName': _name, 'workTask': workTask})
+            return render(request, 'cabinet/private.html', {'myName': _name, 'myData': _data})
         else:
             return redirect('personal_account:general')
     else:
